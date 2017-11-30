a595d9f24a9e9adb5f11eb7c975d6e6ea77fa326
DK\ala <ala@sitecore.net>
2017-11-30 13:44:10 +0200

    Initial class - unchanged

---
+++ src/Sitecore.Support.94858/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94858")]
+[assembly: AssemblyProduct("Sitecore.Support.94858")]
+[assembly: ComVisible(false)]
